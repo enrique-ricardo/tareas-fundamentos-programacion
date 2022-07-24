f412d8761c2bb0cc7529e984d9e38ca052f607bc
Enrique Ricardo <enrique.ricardo@xarxatecactiva.com>
2022-07-24 18:29:43 +0200

    ejercicio_01

---
+++ Ejercicio_01/js/01_app.js
@@ -0,0 +1,12 @@
+
+let number1 = parseInt(prompt('Introduce el primer número' ));
+let number2 = parseInt(prompt('Introduce el segundo número' ));
+
+if( number1 > number2){
+  console.log(`EL NÚMERO MAYOR ES EL: ${number1}`);
+  
+} else if(number2 > number1){
+  console.log(`EL NÚMERO MAYOR ES EL: ${number2}`);
+  
+}
+
